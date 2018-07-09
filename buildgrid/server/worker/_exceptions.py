52a91ef4889298a7eb187aa751752284e83bc295
Ed Baunton <ebaunton1@bloomberg.net>
2018-07-09 17:33:18 -0400

    Fix Exception ErrorDomain for worker

--- buildgrid/server/worker/_exceptions.py
+++ buildgrid/server/worker/_exceptions.py
@@ -4,10 +4,10 @@ from ..._exceptions import BgdError, ErrorDomain
 """
 class InvalidArgumentError(BgdError):
     def __init__(self, message, detail=None, reason=None):
-        super().__init__(message, detail=detail, domain=ErrorDomain.SERVER_WORKER, reason=reason)
+        super().__init__(message, detail=detail, domain=ErrorDomain.SERVER, reason=reason)
 
 """ The worker is out of sync with the server, such as having a differing number of leases
 """
 class OutofSyncError(BgdError):
     def __init__(self, message, detail=None, reason=None):
-        super().__init__(message, detail=detail, domain=ErrorDomain.SERVER_WORKER, reason=reason)
+        super().__init__(message, detail=detail, domain=ErrorDomain.SERVER, reason=reason)
