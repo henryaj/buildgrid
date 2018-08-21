c565b786c5b661602643322ff0fe5f394a826edc
finn <finn.ball@codethink.com>
2018-08-21 14:42:06 +0100

    Updated copyright

--- buildgrid/server/worker/bots_interface.py
+++ buildgrid/server/worker/bots_interface.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2018 Codethink Limited
+# Copyright (C) 2018 Bloomberg LP
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,9 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-# Authors:
-#        Finn Ball <finn.ball@codethink.co.uk>
+
 
 """
 BotsInterface
