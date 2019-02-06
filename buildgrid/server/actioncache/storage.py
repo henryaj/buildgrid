fb81201791fdb796e43eb8eaf176f62e9779b3f3
Martin Blanchard <martin.blanchard@codethink.co.uk>
2019-02-06 17:57:19 +0000

    actioncache/storage.py: Log cache update events
    https://gitlab.com/BuildGrid/buildgrid/issues/156

--- buildgrid/server/actioncache/storage.py
+++ buildgrid/server/actioncache/storage.py
@@ -20,6 +20,7 @@ Action Cache
 Implements an in-memory action Cache
 """
 
+import logging
 
 from ..referencestorage.storage import ReferenceCache
 
@@ -36,19 +37,33 @@ class ActionCache(ReferenceCache):
             cache_failed_actions (bool): cache actions with non-zero exit codes.
         """
         super().__init__(storage, max_cached_refs, allow_updates)
+
+        self.__logger = logging.getLogger(__name__)
+
         self._cache_failed_actions = cache_failed_actions
+
+    # --- Public API ---
 
     def register_instance_with_server(self, instance_name, server):
         server.add_action_cache_instance(self, instance_name)
 
     def get_action_result(self, action_digest):
+        """Retrieves the cached result for an action."""
         key = self._get_key(action_digest)
+
         return self.get_action_reference(key)
 
     def update_action_result(self, action_digest, action_result):
+        """Stores in cache a result for an action."""
         if self._cache_failed_actions or action_result.exit_code == 0:
             key = self._get_key(action_digest)
+
             self.update_reference(key, action_result)
+
+            self.__logger.info("Result cached for action [%s/%s]",
+                               action_digest.hash, action_digest.size_bytes)
+
+    # --- Private API ---
 
     def _get_key(self, action_digest):
         return (action_digest.hash, action_digest.size_bytes)
