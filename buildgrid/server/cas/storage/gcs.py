dc66d95692a9056d60bcd393768f5fface01b740
Henry Stanley <henry@henrystanley.com>
2019-12-05 14:45:57 +0000

    Add logging, implement blob delete in GCS backend

--- buildgrid/server/cas/storage/gcs.py
+++ buildgrid/server/cas/storage/gcs.py
@@ -9,6 +9,7 @@ from .storage_abc import StorageABC
 
 class GCSStorage(StorageABC):
     def __init__(self, bucket, **kwargs):
+        self.__logger.info("Initialised GCS backend")
         self.__logger = logging.getLogger(__name__)
 
         self._client = storage.Client()
@@ -22,10 +23,19 @@ class GCSStorage(StorageABC):
         return True
 
     def get_blob(self, digest):
+        self.__logger.debug("Getting blob: [{}]".format(digest))
         try:
             name = _name_from_digest(digest)
             blob = self._bucket.get_blob(name)
             return io.BytesIO(blob.download_as_string())
+        except NotFound:
+            return None
+
+    def delete_blob(self, digest):
+        self.__logger.debug("Deleting blob: [{}]".format(digest))
+        try:
+            name = _name_from_digest(digest)
+            self._bucket.delete_blob(name)
         except NotFound:
             return None
 
